822005c7215c8926324b9c9475412df68b652920
jane martin <martin.jane.ellis@gmail.com>
2021-05-26 13:31:14 -0400

    Work on full cart component

--- react-app/src/components/Cart.js
+++ react-app/src/components/Cart.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
+import { displayItems } from '../store/cart';
 
 function Cart() {
-
+    const dispatch = useDispatch();
     const userId = useSelector(state => state.session.user.id)
+    const itemList = useSelector(state => {
+        return state.cart.list.map(cartId => state.cart[cartId])
+    });
+    // const allCartItems = ({
+    //     user_id,
+    //     product_id,
+    //     products
+    // }) => dispatch()
+
+     useEffect(() => {
+         dispatch(displayItems(userId));
+     }, [dispatch, userId])
 
     return (
         <div>
-            <h1>{userId}</h1>
+            <h1>{itemList.user_id}</h1>
         </div>
     )
 }
