116c0abe022cdb9429cc604f28b87ae7304e0359
James Sullivan <jsullivan86@gmail.com>
2021-05-29 11:45:21 -0700

    seed data added

--- app/seeds/categories.py
+++ app/seeds/categories.py
@@ -7,11 +7,13 @@ def seed_categories():
     shields = Category(category='Shields')
     armor = Category(category='Armor')
     food_and_drink = Category(category='Food and Drink')
+    unique = Category(category='Unique')
 
     db.session.add(weapons)
     db.session.add(shields)
     db.session.add(armor)
     db.session.add(food_and_drink)
+    db.session.add(unique)
 
     db.session.commit()
 
