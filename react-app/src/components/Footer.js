551ca168fa1b1573e875f0dddc9c00597e2bceb4
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-05-29 14:50:00 -0400

    Made Large changes and fixes to the footer and about sections

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -7,34 +7,35 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-main-container">
                 <div id="footer-dev-title">Developers</div>
+                <NavLink className="footer-dev-name-link" to="/about">Click here for more About the Developers</NavLink>
                 <div className="footer-devs-container">
                     <div className="footer-dev">
                         <NavLink className="footer-dev-name-link" to="/about">
                             <div className="footer-dev-name" >jane Martin</div>
                         </NavLink>
                         <a className="footer-dev-link" href="https://github.com/jemcodes">github</a>
-                        <NavLink className="footer-dev-link" to="https://www.linkedin.com/in/jemcodes/">linkedin</NavLink>
+                        <a className="footer-dev-link" href="https://www.linkedin.com/in/jemcodes/">linkedin</a>
                     </div>
                     <div className="footer-dev">
                         <NavLink className="footer-dev-name-link" to="/about">
                             <div className="footer-dev-name" >Lisa Noor</div>
                         </NavLink>
-                        <NavLink className="footer-dev-link" to="https://github.com/Skulllady">github</NavLink>
-                        <NavLink className="footer-dev-link" to="https://www.linkedin.com/in/lisa-noor-hoque-976120208/">linkedin</NavLink>
+                        <a className="footer-dev-link" href="https://github.com/Skulllady">github</a>
+                        <a className="footer-dev-link" href="https://www.linkedin.com/in/lisa-noor-hoque-976120208/">linkedin</a>
                     </div>
                     <div className="footer-dev">
                         <NavLink className="footer-dev-name-link" to="/about">
                             <div className="footer-dev-name" >Patrick Nusbaum</div>
                         </NavLink>
-                        <NavLink className="footer-dev-link" to="https://github.com/patricknuttree">github</NavLink>
-                        <NavLink className="footer-dev-link" to="www.linkedin.com/in/patrick-nusbaum-mpa">linkedin</NavLink>
+                        <a className="footer-dev-link" href="https://github.com/patricknuttree">github</a>
+                        <a className="footer-dev-link" href="www.linkedin.com/in/patrick-nusbaum-mpa">linkedin</a>
                     </div>
                     <div className="footer-dev">
                         <NavLink className="footer-dev-name-link" to="/about">
                             <div className="footer-dev-name" >Jamie Sullivan</div>
                         </NavLink>
-                        <NavLink className="footer-dev-link" to="https://github.com/bilbopicard">github</NavLink>
-                        <NavLink className="footer-dev-link" to="https://www.linkedin.com/in/sullivan-jamie/">linkedin</NavLink>
+                        <a className="footer-dev-link" href="https://github.com/bilbopicard">github</a>
+                        <a className="footer-dev-link" href="https://www.linkedin.com/in/sullivan-jamie/">linkedin</a>
                     </div>
                 </div>
             </div>
