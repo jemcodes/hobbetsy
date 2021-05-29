7f2ab829b86235550b0bc0c4032a4d86c40a188f
James Sullivan <jsullivan86@gmail.com>
2021-05-29 09:08:56 -0700

    Added search styling

--- react-app/src/components/SingleSearchItem.js
+++ react-app/src/components/SingleSearchItem.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import Search from './Search';
 
 
-function SingleSearchItem({ product }){
-    
+
+function SingleSearchItem({ product, setSearch }) {
+
+    const history = useHistory()
+    const clearSearch = (e) => {
+        e.preventDefault()
+        setSearch('')
+        history.push(`/products/${product.id}`)
+    }
+
     return (
-        <div>
-                <div><Link to={`/products/${product.id}`}>{product.title}</Link></div>
+        <div className='populated-search-results'>
+            <Link to={`/products/${product.id}`} onClick={clearSearch}>{product.title}</Link>
         </div>
     )
 }
